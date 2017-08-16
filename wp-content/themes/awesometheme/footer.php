cf2efd21edc93f79df73d106fb69ddd903355ce8
henrymbuguak <henrymbuguak@gmail.com>
2017-08-16 10:13:53 +0300

    Bootstrap navigation functionality

--- wp-content/themes/awesometheme/footer.php
+++ wp-content/themes/awesometheme/footer.php
@@ -3,7 +3,7 @@
      <p>This is my footer</p>
      <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
    </footer>
-
+</div><!-- Close container -->
    <?php wp_footer(); ?>
   </body>
 </html>
--- wp-content/themes/awesometheme/header.php
+++ wp-content/themes/awesometheme/header.php
@@ -15,5 +15,35 @@
    ?>
 
   <body <?php body_class($awesome_classes); ?>>
-    <?php wp_nav_menu(array('theme_location' => 'primary'));?>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <!-- Navigation Menu -->
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">HenryLab Theme</a>
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <?php
+                   wp_nav_menu(array(
+                     'theme_location' => 'primary',
+                     'container'=>false,
+                     'menu_class' => 'nav navbar-nav navbar-right'
+                   )
+                 );
+                ?>
+              </div>
+            </div>
+          </nav>
+          <!-- End Menu -->
+
+        </div>
+      </div>
+
     <img src="<?php header_image(); ?>" alt="" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>">
